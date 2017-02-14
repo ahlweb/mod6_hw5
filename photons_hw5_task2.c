5bba0049e462f6329ffd10aa9c2ee7944c3f57c0
ahlweb <adamlee1@mail.weber.edu>
2017-02-14 16:58:28 +0000

    description change

--- photons_hw5_task2.c
+++ photons_hw5_task2.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  photons_hw5_task2.c
  *       	Usage:  ./photons_hw5_task2.c
- *    Description: 
+ *    Description: adams task 
  *
  *        Version:  1.0
  *        Created:  02/14/2017 04:38:26 PM
