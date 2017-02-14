50ca8022ef0b1a001a3b431cf92c40ec3eae50ac
ahlweb <adamlee1@mail.weber.edu>
2017-02-14 16:39:51 +0000

    initail commit

---
+++ photons_hw5_task2.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  photons_hw5_task2.c
+ *       	Usage:  ./photons_hw5_task2.c
+ *    Description: 
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:38:26 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Adam Lee (), Adamlee1@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+
+/* Main Program */
+int main(void)
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
