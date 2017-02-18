d8b57935d2278af0151f587190c98e457050d420
ahlweb <adamlee1@mail.weber.edu>
2017-02-18 04:54:44 +0000

    task 2 finished

--- photons_hw5_task2.c
+++ photons_hw5_task2.c
@@ -13,14 +13,48 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 
+float MakeMilesKmTable(float k);
 /* Main Program */
 int main(void)
 {
 
+	float inc, km;
+	int end, miles;
+	printf("Enter the starting miles: ");
+	scanf("%d", &miles);
+
+	printf("Enter the ending miles: ");
+	scanf("%d", &end);
+
+	printf("Enter the incraments: ");
+	scanf("%f", &inc);
+
+	printf("Miles  =   Kilometers\n");
+
+	while (miles <= end)
+	{
+
+		km = MakeMilesKmTable(miles);
+		printf("%3d        %5.2f\n", miles, km); 
+
+		miles += inc;
+	}
+
+
+
+
 	return 0;
+
+
 }
 
 
 /* Function Defenitions */
 
+float MakeMilesKmTable(float k)
+{
+float f;
+f= k * 1.61;
+return f;
+}
 
